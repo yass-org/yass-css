d7c058cf092f3d828e015e51799ccb8e773fb404
Sam Scheding <sscheding@atlassian.com>
2023-07-03 22:57:30 +1000

    YASS-15: Allow write perms in pipeline

--- __tests__/e2e.spec.ts
+++ __tests__/e2e.spec.ts
@@ -3,13 +3,14 @@ import path from 'path'
 import spawn from 'spawndamnit'
 
 describe('yass', () => {
+  beforeAll(() => {
+    spawn('npm', ['i', '--location=global'])
+  })
+
   it('works as expected with default config',  async () => {
     const cwd = path.join(__dirname, 'fixtures/projects/1-no-config')
 
-    const child = spawn('npx', ['yass-css'], { cwd })
-    const { stdout, stderr } = await child
-    console.log('stdout', stdout.toString())
-    console.log('stderr', stderr.toString())
+    await spawn('npx', ['yass-css'], { cwd })
 
     await expect(fs.existsSync(path.join(cwd, '/styles/yass/yass.css'))).toBe(true)
 
