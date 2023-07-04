729d42113b0312b496b4a7a7f43185c0f523892f
Sam Scheding <sscheding@atlassian.com>
2023-07-04 10:46:59 +1000

    YASS-15: Allow write perms in pipeline

--- __tests__/e2e.spec.ts
+++ __tests__/e2e.spec.ts
@@ -3,20 +3,15 @@ import path from 'path'
 import spawn from 'spawndamnit'
 
 describe('yass', () => {
-  beforeAll(async () => {
-    const { stdout }  = await spawn('ls', ['-al'], { cwd: __dirname.replace('__tests__', '') })
-    console.log('stdout', stdout.toString())
-
-    await spawn('npm', ['install', '--location=global'], { cwd: __dirname.replace('__tests__', '') })
-  })
 
   it('works as expected with default config',  async () => {
-    const cwd = path.join(__dirname, 'fixtures/projects/1-no-config')
+    const testsDir = __dirname
+    const testProjectDir = path.join(testsDir, 'fixtures/projects/1-no-config')
 
-    await spawn('npx', ['yass-css'], { cwd })
+    await spawn('../../../../build/index.js', [], { cwd: testProjectDir })
 
-    await expect(fs.existsSync(path.join(cwd, '/styles/yass/yass.css'))).toBe(true)
+    await expect(fs.existsSync(path.join(testProjectDir, '/styles/yass/yass.css'))).toBe(true)
 
-    await spawn('rm', ['-rf', path.join(cwd, 'styles')], { cwd })
+    await spawn('rm', ['-rf', path.join(testProjectDir, 'styles')], { cwd: testProjectDir })
   })
 })
