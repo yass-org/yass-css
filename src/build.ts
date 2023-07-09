26b5dcf12adb72b9169ef7b6ddb12cf2dec8b85e
Sam Scheding <sscheding@atlassian.com>
2023-07-10 00:20:12 +1000

    YASS-28: improve tokenisation of source folder

--- src/build.ts
+++ src/build.ts
@@ -14,6 +14,7 @@ export const build = ({ tokens, sourceSet, config }: {tokens: DesignToken[], sou
 
   const isInSourceDirectory = (atomicClass: AtomicClass) => {
     if(!sourceSet) {
+
       return true
     }
 
