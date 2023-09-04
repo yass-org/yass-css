d66f8925d4f2a69101d6b359fb44a53f8e18104c
Sam Scheding <sscheding@atlassian.com>
2023-09-04 10:17:55 +1000

    NO-ISSUE: Add tests

--- __tests__/package.spec.ts
+++ __tests__/package.spec.ts
@@ -1,15 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageJSON = require('../package.json')
+import { findRootSync } from '@manypkg/find-root'
 import fs from 'fs'
+import path from 'path'
 
 describe('package.json', () => {
   it('"main" field is correct', () => {
     // "main" field is the entrypoint for using `yass-css` programmatically
     // it allows `import yass from 'yass-css'`
     const expected = './build/index.js'
+    const { rootDir } = findRootSync(__dirname)
+    const fullPath = path.join(rootDir, expected)
 
     expect(packageJSON.main).toBe(expected)
-    expect(fs.existsSync(expected)).toBe(true)
+    expect(fs.existsSync(fullPath)).toBe(true)
   })
 
   describe('bin', ()=> {
@@ -17,9 +21,11 @@ describe('package.json', () => {
       // "bin['yass-css']" field is the entrypoint for running `yass-css` in CLI
       // it allows `npx yass-css`
       const expected = './build/build.js'
+      const { rootDir } = findRootSync(__dirname)
+      const fullPath = path.join(rootDir, expected)
 
       expect(packageJSON.bin['yass-css']).toBe(expected)
-      expect(fs.existsSync(expected)).toBe(true)
+      expect(fs.existsSync(fullPath)).toBe(true)
     })
   })
 
